07a4a8ed621c78b2da721f26200f18d008350cd5
stayforyou <bread.s@kakao.com>
2024-04-12 23:54:49 +0900

    QueryDSL 설정 추가

--- module-persistence/rdbms/build.gradle.kts
+++ module-persistence/rdbms/build.gradle.kts
@@ -9,6 +9,9 @@ dependencies {
 
     api("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
 }
--- module-persistence/rdbms/src/main/kotlin/io/soboro/supreme/persistence/rdbms/jpa/ProductJpaRepository.kt
+++ module-persistence/rdbms/src/main/kotlin/io/soboro/supreme/persistence/rdbms/jpa/ProductJpaRepository.kt
@@ -1,12 +1,13 @@
 package io.soboro.supreme.persistence.rdbms.jpa
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import io.soboro.supreme.core.model.product.entity.Product
 import io.soboro.supreme.core.model.product.enums.ProductType
 import io.soboro.supreme.core.repository.ProductRepository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-interface ProductJpaRepository : JpaRepository<Product, Long>
+interface ProductJpaRepository : JpaRepository<Product, Long>, CustomProductRepository
 
 @Repository
 class ProductRepositoryAdapter(
@@ -15,4 +16,18 @@ class ProductRepositoryAdapter(
     override fun findAll(type: ProductType): List<Product> {
         return productJpaRepository.findAll()
     }
+}
+
+@Repository
+class CustomProductRepositoryImpl(
+    private val queryFactory: JPAQueryFactory
+) : CustomProductRepository {
+
+    override fun findAll(): List<Product> {
+        return emptyList()
+    }
+}
+
+interface CustomProductRepository {
+    fun findAll(): List<Product>
 }
